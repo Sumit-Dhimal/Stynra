911045ee80b90e522afff1846e916ea4c53ec179
Sumit Dhimal <sumit.dhimal.333@gmail.com>
2025-09-24 22:13:06 +0545

    C:/Program Files/Git/api is directly called using proxy

--- server/src/server.js
+++ server/src/server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // routes
-app.use('/api/users', userRoute);
+app.use('/users', userRoute);
 
 
 app.get('/api', (req, res) => {
